fd3a7ee56782639fc4d6d06e0e58003c1939a30a
chiakiaosima <starsrock5937@gmail.com>
2021-08-06 19:10:55 +0900

    first_commit

---
+++ MyCamera/ImagePickerView.swift
@@ -0,0 +1,20 @@
+//
+//  ImagePickerView.swift
+//  MyCamera
+//
+//  Created by 青島智明 on 2021/08/06.
+//
+
+import SwiftUI
+
+struct ImagePickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImagePickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImagePickerView()
+    }
+}
