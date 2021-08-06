401c2bc4728cb831814bcf822d03929312bb063b
chiakiaosima <starsrock5937@gmail.com>
2021-08-06 19:08:00 +0900

    Initial Commit

---
+++ MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by 青島智明 on 2021/08/06.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
